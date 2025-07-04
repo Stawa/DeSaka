c82667d346a2ce93c031578afe149c53ecd53329
Stawa <damodaraswarupa@gmail.com>
2025-07-04 15:20:54 +0800

    Redesign HomeView Components

--- src/components/PlantStatusDashboard.vue
+++ src/components/PlantStatusDashboard.vue
@@ -166,7 +166,7 @@ const currentTime = new Date().toLocaleString('en-US', {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
-          <h2 class="font-semibold text-gray-900 dark:text-gray-100">
+          <h2 class="font-semibold text-gray-900 dark:text-white">
             System Information
           </h2>
         </div>
@@ -220,7 +220,7 @@ const currentTime = new Date().toLocaleString('en-US', {
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
             </svg>
           </div>
-          <h2 class="font-semibold text-gray-900 dark:text-gray-100">
+          <h2 class="font-semibold text-gray-900 dark:text-white">
             Connected Sensors
           </h2>
         </div>
